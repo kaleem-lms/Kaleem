7455c61fb24f14f2700f5603b19ccddb30448bff
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2024-12-16 13:52:57 +0200

    Add: inital messaging app

--- kaleem/config/api_router.py
+++ kaleem/config/api_router.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from kaleem.messaging.api.views import ChatGroupViewSet
+from kaleem.messaging.api.views import ChatMessageMediaViewSet
+from kaleem.messaging.api.views import ChatMessageViewSet
 from kaleem.users.api.views import AuthenticationViewSet
 from kaleem.users.api.views import UserViewSet
 
@@ -9,6 +12,9 @@ router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
 router.register("authentication", AuthenticationViewSet)
+router.register("chat-group", ChatGroupViewSet)
+router.register("chat-message", ChatMessageViewSet)
+router.register("chat-message-media", ChatMessageMediaViewSet)
 
 
 app_name = "api"
