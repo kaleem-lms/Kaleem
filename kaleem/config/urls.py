43477853022d55ab5cb17d039cdd3771601aa2ef
Abdulkhalek-1 <abdulkhalek.muhmmad.work@gmail.com>
2025-03-20 18:16:32 +0200

    Restore home and about page URL patterns in the Django configuration

--- kaleem/config/urls.py
+++ kaleem/config/urls.py
@@ -21,12 +21,12 @@ def health_check(request):
 
 urlpatterns = [
     path("health/", health_check),
-    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    # path(
-    #     "about/",
-    #     TemplateView.as_view(template_name="pages/about.html"),
-    #     name="about",
-    # ),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "about/",
+        TemplateView.as_view(template_name="pages/about.html"),
+        name="about",
+    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
